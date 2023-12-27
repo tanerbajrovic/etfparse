ee6ec7db35a2bee0e32ccbfcc7e26f7114cb48c1
Taner Bajrovic <tanerbajrovic@gmail.com>
2023-12-27 15:13:28 +0100

    Add exception handling for common file issues

--- etf-parse.py
+++ etf-parse.py
@@ -3,34 +3,38 @@
 import json
 import argparse
 
+__version__ = '1.0.0'
 
 # TODO: Add support for above_main and main differentiation
 def main(filename: str):
-    fileJSON = get_json_file(filename)
-    name = fileJSON['name']
-    length = len(fileJSON['tests'])
-    fileOutput = open(filename + '-output.txt', 'w')
-    for i in range(1, length):
-        code = fileJSON['tests'][i]['patch'][0]['code']
-        fileOutput.write(f'// AT {i}' + '\n')
-        fileOutput.write(code + '\n\n')
-    print('Success: ' + f'"{name}"' + '\n')
-    fileOutput.close()
+    json_file = get_json_file(filename)
+    homework_name = json_file['name']
+    total_tests = len(json_file['tests'])
+    try:
+        with open(filename + '-output.txt', 'w') as output:
+            for test_number in range(1, total_tests):
+                code = json_file['tests'][test_number]['patch'][0]['code']
+                output.write(f'// AT {test_number}' + '\n')
+                output.write(code + '\n\n')
+            print('Success: ' + f'"{homework_name}"' + '\n')
+    except:
+        print('Unable to create an output file')
+        exit(2)
 
 
 # TODO: Add error-checking if file doesn't exist
 def get_json_file(filename: str):
     try:
-        with open(filename, 'r') as file:
-            fileJSON = json.load(file)
+        with open(filename, 'r') as input:
+            json_file = json.load(input)
+        return json_file
     except:
         print(
-            f'Error opening the file {filename} -- possibly not in JSON format')
+            f'Error opening {filename} -- check if it is in JSON format!')
         exit(1)
-    return fileJSON
 
 
-# TODO: Add error-checking if file doesn't exist
+# TODO: Don't require user to pass filename -> default to 'autotest2'
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('filename', default='autotest2',
