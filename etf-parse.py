ce892678d87beb2af090523a1f429960d8bc034d
Taner Bajrovic <tanerbajrovic@gmail.com>
2024-01-08 23:32:49 +0100

    Fix argparser to use default filename and add `expected_output` to test output

--- etf-parse.py
+++ etf-parse.py
@@ -5,6 +5,7 @@ import argparse
 
 __version__ = '1.0.0'
 
+
 # TODO: Add support for above_main and main differentiation
 def main(filename: str):
     json_file = get_json_file(filename)
@@ -14,15 +15,16 @@ def main(filename: str):
         with open(filename + '-output.txt', 'w') as output:
             for test_number in range(1, total_tests):
                 code = json_file['tests'][test_number]['patch'][0]['code']
+                expected_output = json_file['tests'][test_number]['execute']['expect'][0]
                 output.write(f'// AT {test_number}' + '\n')
                 output.write(code + '\n\n')
+                output.write(f'Expected: {expected_output}' + '\n\n\n')
             print('Success: ' + f'"{homework_name}"' + '\n')
     except:
         print('Unable to create an output file')
         exit(2)
 
 
-# TODO: Add error-checking if file doesn't exist
 def get_json_file(filename: str):
     try:
         with open(filename, 'r') as input:
@@ -34,10 +36,10 @@ def get_json_file(filename: str):
         exit(1)
 
 
-# TODO: Don't require user to pass filename -> default to 'autotest2'
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('filename', default='autotest2',
-                        type=argparse.FileType('r'))
+    parser.add_argument('filename', nargs='?', default='autotest2',
+                        help='Input the name of your autotest file',
+                        type=argparse.FileType('r', encoding='UTF-8'))
     args = parser.parse_args()
     main(args.filename.name)
