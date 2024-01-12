efb1807ebe5f9aeb0f4ee0599c2f07ba07db57d1
Taner Bajrovic <tanerbajrovic@gmail.com>
2024-01-12 11:36:47 +0100

    Add explicit exception-handling and update naming

--- etf-parse.py
+++ etf-parse.py
@@ -3,37 +3,39 @@
 import json
 import argparse
 
+
 __version__ = '1.0.0'
 
 
-# TODO: Add support for above_main and main differentiation
-def main(filename: str):
+def main(filename: str) -> None:
     json_file = get_json_file(filename)
     homework_name = json_file['name']
     total_tests = len(json_file['tests'])
     try:
-        with open(filename + '-output.txt', 'w') as output:
+        with open(filename + '-output.txt', 'w') as output_file:
             for test_number in range(1, total_tests):
                 code = json_file['tests'][test_number]['patch'][0]['code']
                 expected_output = json_file['tests'][test_number]['execute']['expect'][0]
-                output.write(f'// AT {test_number}' + '\n')
-                output.write(code + '\n\n')
-                output.write(f'Expected: {expected_output}' + '\n\n\n')
+                output_file.write(f'// AT {test_number}' + '\n')
+                output_file.write(code + '\n\n')
+                output_file.write(f'Expected: {expected_output}' + '\n\n\n')
             print('Success: ' + f'"{homework_name}"' + '\n')
-    except:
-        print('Unable to create an output file')
-        exit(2)
+    except PermissionError:
+        print('Error: Unable to create an output file (Insufficient permissions)')
+        exit(3)
 
 
 def get_json_file(filename: str):
     try:
-        with open(filename, 'r') as input:
-            json_file = json.load(input)
+        with open(filename, 'r') as input_file:
+            json_file = json.load(input_file)
         return json_file
-    except:
-        print(
-            f'Error opening {filename} -- check if it is in JSON format!')
+    except PermissionError:
+        print('Error: Unable to read file (Insufficient permissions)')
         exit(1)
+    except json.JSONDecodeError:
+        print('Error: Unable to read file (Not in JSON format)')
+        exit(2)
 
 
 if __name__ == '__main__':
